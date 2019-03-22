0416337854c8d51b0de2fea40d15da948b25f312
kennyS <48831014+1120349140@users.noreply.github.com>
2019-03-22 21:54:06 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+package cn.kgc.kd61.lj.cha1.p1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("姓名\t性别\t年龄\t地址");
+		System.out.println("张三\t女\t9\t南京鼓楼");
+		System.out.println("张三丰\t男\t19\t北京天安门");
+	}
+
+}
